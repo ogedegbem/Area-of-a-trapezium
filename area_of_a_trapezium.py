200db77f58af4d79ef28ce2cb4685bf7c8f7543a
ogedegbem <83469006+ogedegbem@users.noreply.github.com>
2023-01-06 14:03:53 +0100

    Add files via upload

---
+++ area_of_a_trapezium.py
@@ -0,0 +1,6 @@
+def areaofTrap(base1, base2, height):
+    """Compute the sum of the squares of numbers from 1 to n."""
+    area = ((base1 + base2) * height)/2
+    return area
+    
+    raise NotImplementedError()
